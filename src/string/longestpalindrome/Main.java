b0b23977b8314cdbb47215ee3e9753127cbff98d
reza3524 <shams.reza1375@gmail.com>
2023-10-20 21:43:35 +0330

    Add some algorithm.

---
+++ src/string/longestpalindrome/Main.java
@@ -0,0 +1,30 @@
+package string.longestpalindrome;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("babad"));
+    }
+
+    static int start = 0;
+    static int end = 0;
+    public static String longestPalindrome(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            handlePalindrome(s, i, i);
+            handlePalindrome(s, i, i + 1);
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static void handlePalindrome(String string, int left, int right) {
+        while (left >= 0 && right < string.length() && string.charAt(left) == string.charAt(right)) {
+            left -= 1;
+            right += 1;
+        }
+        left += 1;
+        right -= 1;
+        if (end - start + 1 < right - left + 1) {
+            start = left;
+            end = right;
+        }
+    }
+}
--- src/string/validparentheses/Main.java
+++ src/string/validparentheses/Main.java
@@ -1,14 +1,33 @@
 package string.validparentheses;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isValid(" "));
+        System.out.println(isValid("([)]"));
     }
 
     public static boolean isValid(String s) {
-        if (s.trim().isEmpty()) {
-            return false;
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i <s.length() ; i++) {
+            char current = s.charAt(i);
+            if (map.containsKey(current)) {
+                char pop = !stack.isEmpty() ? stack.pop() : '#';
+                if (pop != map.get(current)) {
+                    return false;
+                }
+            } else {
+                stack.push(current);
+            }
         }
-       return true;
+        return stack.isEmpty();
     }
 }
